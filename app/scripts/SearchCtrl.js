d9cd433a92b4525c51c78c3581e5e582f3f7d20c
pepegl <ppetrea@insynctive.com>
2013-12-12 17:41:27 +0200

    remove unused code

--- app/scripts/SearchCtrl.js
+++ app/scripts/SearchCtrl.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function SearchCtrl($scope, $location, $route, $http, $filter)
+function SearchCtrl($scope, $location, $route, $http)
 {
     $scope.endyear = 2013;
     $scope.year = ($route.current.params.year ? $route.current.params.year : null);
@@ -65,22 +65,6 @@ function SearchCtrl($scope, $location, $route, $http, $filter)
         $scope.change($scope.endyear - 1, "FY");
     }
 
-    $scope.SetCik = function ()
-    {
-        if (event.keyCode == 13 && $scope.entity)
-            $filter('filter')($scope.entities, function (elem)
-            {
-                if ($scope.entity == elem.name)
-                {
-                    $scope.Cik = elem.cik;
-                    return;
-                }
-
-            });
-
-
-    }
-
     $scope.getEntities();
 
     $scope.update = function (element)
@@ -98,48 +82,4 @@ function SearchCtrl($scope, $location, $route, $http, $filter)
 
     };
 
-    /* $scope.$watch('name', function (value)
-     {
-     $scope.entities.filter(function (e)
-     {
-     if (e.name == value)
-     {
-     $scope.name = e.name;
-     $scope.cik = e.cik;
-     $scope.ticker = e.ticker;
-     return;
-     }
-     })
-
-     });
-
-     $scope.$watch('cik', function (value)
-     {
-     $scope.entities.filter(function (e)
-     {
-     if (e.cik == value)
-     {
-     $scope.name = e.name;
-     $scope.cik = e.cik;
-     $scope.ticker = e.ticker;
-     return;
-     }
-     })
-
-     });
-
-     $scope.$watch('ticker', function (value)
-     {
-     $scope.entities.filter(function (e)
-     {
-     if (e.ticker == value)
-     {
-     $scope.name = e.name;
-     $scope.cik = e.cik;
-     $scope.ticker = e.ticker;
-     return;
-     }
-     })
-
-     });*/
 }
