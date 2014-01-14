80e5ca9f1ad980e5770724a3c87f9cfe3e643dd2
Matthias Brantner <brantner@Matthiass-MacBook-Air.local>
2014-01-13 16:21:35 -0800

    added /browser endpoint for browsing the database

---
+++ app/scripts/BrowserCtrl.js
@@ -0,0 +1,27 @@
+"use strict"
+
+function BrowserCtrl($scope, $http, API_URL) {
+  $scope.results = [];
+  $scope.api_url = API_URL.substring(0,API_URL.length-3);
+  $scope.getdata = function() {
+    $http(
+      {
+        method : 'POST',
+        url: API_URL + '/_queries/public/entities.jq',
+      }).
+      success(function(data, status, headers, config) {
+        $scope.results = data.entities;
+        $scope.pageCount = 50;
+        $scope.safeApply();
+      }).
+      error(function(data, status, headers, config) {
+      });
+  };
+  $scope.getmore = function() {
+    $scope.pageCount += 50;
+  };
+  $scope.getall = function() {
+    $scope.pageCount = $scope.results.length;
+  };
+  $scope.getdata();
+};
