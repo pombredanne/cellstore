0b592ebfbfdc04d101750cabc6ca7cc03ad116d2
Matthias Brantner <brantner@Matthiass-MacBook-Air.local>
2014-01-22 14:46:00 -0800

    fixed some stuff fore components

--- app/scripts/ComponentsCtrl.js
+++ app/scripts/ComponentsCtrl.js
@@ -4,6 +4,8 @@ function ComponentsCtrl($scope, $route, $http, API_URL) {
   $scope.results = [];
   $scope.API_URL = API_URL;
   $scope.AccessionNumber = $route.current.params.accession;
+  $scope.cik = "";
+  $scope.EntityRegistrantName = "";
   $scope.getdata = function() {
     $http(
       {
@@ -11,13 +13,24 @@ function ComponentsCtrl($scope, $route, $http, API_URL) {
         url: API_URL + '/_queries/public/components.jq?accession=' + $scope.AccessionNumber,
       }).
       success(function(data, status, headers, config) {
-        $scope.results = data;
+        $scope.results = data.Components;
+        $scope.cik = data.CIK;
+        $scope.EntityRegistrantName = data.EntityRegistrantName;
         $scope.safeApply();
       }).
       error(function(data, status, headers, config) {
       });
   };
   $scope.getdata();
-};
+  $scope.trimTableURL = function(url) {
+    if (url.length < 30) return url;
 
+    return url.substr(0, 10) + "..." + url.substr(url.length - 20);
+  }
+  $scope.trimNetworkURL = function(url) {
+    if (url.length < 50) return url;
+
+    return url.substr(0, 20) + "..." + url.substr(url.length - 30);
+  }
+};
 
