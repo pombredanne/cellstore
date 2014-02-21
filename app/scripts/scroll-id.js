d9be6f9920a497ea8906d55899919ce66b93b001
Cristi Dumitru <cdumitru@insynctive.com>
2014-02-21 12:54:28 -0800

    Added animations and homepage changes

--- app/scripts/scroll-id.js
+++ app/scripts/scroll-id.js
@@ -5,8 +5,8 @@ angular.module('scroll-id', [])
 	return function (scope, element, attributes) {
 	  scope.$on('scroll-id', function(event, id) {
 		  if (id == attributes.scrollId) {
-			window.scrollTo(0, element[0].getBoundingClientRect().top)
-		  }
+			$('html, body').animate({ scrollTop: element[0].getBoundingClientRect().top }, 1000);
+		   }
 		});
 	}
 });
