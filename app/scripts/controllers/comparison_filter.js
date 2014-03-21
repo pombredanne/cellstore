4192de41b59ed0a80b7d2cb359b067f31af596d6
William Candillon <wcandillon@gmail.com>
2014-03-21 15:11:13 +0100

    Fix warnings

--- app/scripts/controllers/comparison_filter.js
+++ app/scripts/controllers/comparison_filter.js
@@ -1,5 +1,7 @@
-angular.module('main').controller('ComparisonFilterCtrl', ['$scope', '$route', '$angularCacheFactory', '$backend', 'QueriesService',
-  function($scope, $route, $angularCacheFactory, $backend, QueriesService) {
+'use strict';
+
+angular.module('main')
+.controller('ComparisonFilterCtrl', function($scope, $route, $angularCacheFactory, $backend, QueriesService) {
     $scope.service = (new QueriesService($backend.API_URL + '/_queries/public/api'));
     $scope.tags = [];
     $scope.entities = [];
@@ -10,35 +12,35 @@ angular.module('main').controller('ComparisonFilterCtrl', ['$scope', '$route', '
         $scope.tags = t;
         $backend.getEntities().then(function(e) {
             $scope.entities = e;
-            $backend.getYears().then(function(y) { 
+            $backend.getYears().then(function(y) {
                 $scope.years = y;
                 $backend.getPeriods().then(function(p) {
                     $scope.periods = p;
                     //focus automatically on last year and FY
-                    $scope.selection = $angularCacheFactory.get('secxbrl').get('selection') 
-                                    || { entity: [], tag: [ 'DOW30' ], year: [ $scope.years[1] ], period: [ $scope.periods[0] ] };
+                    $scope.selection = $angularCacheFactory.get('secxbrl').get('selection') ||
+                                        { entity: [], tag: [ 'DOW30' ], year: [ $scope.years[1] ], period: [ $scope.periods[0] ] };
                 });
             });
         });
     });
 
-    $scope.selectEntity = function(entity) { 
-        if ($scope.selection.entity.indexOf(entity) < 0)
+    $scope.selectEntity = function(entity) {
+        if ($scope.selection.entity.indexOf(entity) < 0) {
             $scope.selection.entity.push(entity);
+        }
         $scope.name = null;
     };
 
-    $scope.reset = function() { 
+    $scope.reset = function() {
         $angularCacheFactory.get('secxbrl').remove('selection');
         $route.reload();
     };
 
     $scope.$watch(
-        function() { return angular.toJson($scope.selection) },
-        function() { 
+        function() { return angular.toJson($scope.selection); },
+        function() {
             $angularCacheFactory.get('secxbrl').put('selection', $scope.selection);
-            $scope.$emit('filterChanged', $scope.selection); 
+            $scope.$emit('filterChanged', $scope.selection);
         }
     );
-  }
-]);
+});
