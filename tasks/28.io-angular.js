ee11913d8f00613dd3c3dcde16ca137a21cb8c43
Cristi Dumitru <cdumitru@insynctive.com>
2014-04-25 17:25:23 -0700

    Implement optimizations in GruntFile

--- tasks/28.io-angular.js
+++ tasks/28.io-angular.js
@@ -7,21 +7,23 @@ module.exports = function(grunt) {
        
         var done = this.async();
         var options = this.options();
+        var url = this.data.url;
         var dest = options.dest;
         grunt.file.mkdir(dest);
      
         var count = options.apis.length;
+        grunt.log.writeln('Using api: ' + url);
         options.apis.forEach(function(api, index){
             var swagger = fs.readFileSync(api.swagger);
             request({
-                uri: 'http://secxbrl-beta.xbrl.io/angular.jq',
+                uri: url + '/angular.jq',
                 qs: { module: api.module, service: api.service, 'new-module': api.newModule },
                 headers: { 'Content-Type': 'text/json; utf-8' },
                 body: swagger
             }, function(error, response, body){
                 if(response.statusCode !== 200) {
-                    grunt.fail.fatal("Sever replied with: " + response.statusCode);  
-                } 
+                    grunt.fail.fatal("Server replied: " + response.statusCode);
+                }
                 fs.writeFileSync(dest + '/' + api.service + '.js', body);
                 grunt.log.writeln(dest + '/' + api.service + '.js written (' + api.module + '.' + api.service + ')');
                 count--;
