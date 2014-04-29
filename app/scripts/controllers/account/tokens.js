b3678a0707fd55ba213adffccd90e6c140880538
Cristi Dumitru <cdumitru@insynctive.com>
2014-04-29 15:18:59 -0700

    Addressed review comments

--- app/scripts/controllers/account/tokens.js
+++ app/scripts/controllers/account/tokens.js
@@ -2,12 +2,11 @@
 
 angular.module('main')
 .controller('AccountTokensCtrl', function($scope, $modal, $backend, SessionService) {
-    
+    $scope.tokens = [];
     $scope.service = new SessionService($backend.API_URL + '/_queries/public');
     
     $scope.getData = function() {
-        $scope.tokens = [];
-        $scope.service.listTokens({ $method: 'POST', token: $scope.token, $refresh: true })
+        $scope.service.tokens({ $method: 'POST', token: $scope.token, $refresh: true })
             .then(
                 function(data) {
                     $scope.tokens = data.results;
@@ -20,6 +19,7 @@ angular.module('main')
     
     $scope.revoke = function(token) {
         $modal.open( {
+            windowClass: 'modal-small',
             templateUrl: 'tokens_revoke.html',
             controller: ['$scope', '$modalInstance', 'token',  function ($scope, $modalInstance, token) {
                 $scope.object = { token: token, password: '' };
@@ -34,7 +34,7 @@ angular.module('main')
                 token: function() { return token.token; }
             }
         }).result.then(function(item) {
-            $scope.service.revokeToken({ $method: 'POST', email: $scope.user.email, password: item.password, token: token.token, $refresh: true })
+            $scope.service.revoke({ $method: 'POST', email: $scope.user.email, password: item.password, token: token.token, $refresh: true })
                 .then(
                     function(data) {
                         if(data && data.success) {
@@ -52,20 +52,22 @@ angular.module('main')
 
     $scope.create = function() {
         $modal.open( {
+            windowClass: 'modal-small',
             templateUrl: 'tokens_create.html',
             controller: ['$scope', '$modalInstance',  function ($scope, $modalInstance) {
                 var d = new Date();
-                d.setDate(d.getDate() + 3);
-                $scope.object = { expiration: d, password: '' };
+                d.setFullYear(d.getFullYear() + 1);
+                $scope.object = { date: d, password: '' };
                 $scope.cancel = function () {
                     $modalInstance.dismiss('cancel');
                 };
                 $scope.ok = function () {
-                    $modalInstance.close({ password: $scope.object.password, expiration: $scope.object.expiration.toISOString() });
+                    var d = new Date($scope.object.date);
+                    $modalInstance.close({ password: $scope.object.password, expiration: d.toISOString() });
                 };
             }]
         }).result.then(function(item) {
-            $scope.service.newToken({ $method: 'POST', email: $scope.user.email, password: item.password, expiration: item.expiration, $refresh: true })
+            $scope.service.token({ $method: 'POST', email: $scope.user.email, password: item.password, expiration: item.expiration, $refresh: true })
                 .then(
                     function(data) {
                         if(data && data.success) {
