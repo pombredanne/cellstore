d47c7741342b2e1a9f3b1536c504fe3324c656de
Cristi Dumitru <cdumitru@insynctive.com>
2014-07-03 14:55:17 -0700

    adress review changes

--- tasks/28.io-nggettext.js
+++ tasks/28.io-nggettext.js
@@ -84,4 +84,47 @@ module.exports = function(grunt) {
         });
         done();
     });
+    grunt.registerMultiTask('nggettext_merge', 'Merge a translation file in a template', function(){
+
+        var done = this.async();
+        var template = this.data.template;
+        var langfiles = this.data.langfiles;
+        
+        grunt.log.writeln('Processing template: ' + template);
+        var contentTemplate = grunt.file.read(template);
+        var reg = /(?:\r\n|\r|\n)(#: (.*)(?:\r\n|\r|\n))+msgid "(.*)"(?:\r\n|\r|\n)msgstr "(.*)"(?:\r\n|\r|\n)/gi;
+        var matches = [];
+        var match = reg.exec(contentTemplate);
+        while (match !== null) {
+            matches.push({ key: match[3], value: match[0] });
+            match = reg.exec(contentTemplate);
+        }
+        grunt.file.expand(langfiles).forEach(function(file) {
+            grunt.log.writeln('--------------------------------');
+            grunt.log.writeln('Processing file: ' + file);
+            var contentFile = grunt.file.read(file);
+            
+            var mergedFile = 'msgid ""\nmsgstr ""\n"Content-Type: text/plain; charset=UTF-8\\n"\n"Content-Transfer-Encoding: 8bit\\n"\n"Language: ' + 
+                    file.substring(file.lastIndexOf('/') + 1, file.length - 3) + '\\n"\n';
+
+            matches.forEach(function(match) {
+                var r = new RegExp('(?:\r\n|\r|\n)msgid "' + match.key
+                                .replace(/"/g, '\\"').replace(/\(/g, '\\(').replace(/\)/g, '\\)')
+                                .replace(/\[/g, '\\[').replace(/\]/g, '\\]').replace(/\?/g, '\\?')
+                                .replace(/\$/g, '\\$').replace(/\^/g, '\\^').replace(/\|/g, '\\|') +
+                                '"(?:\r\n|\r|\n)msgstr "(.*)"(?:\r\n|\r|\n)');
+                var m = r.exec(contentFile);
+                if (m !== null)
+                {
+                    mergedFile += match.value.replace('msgstr ""', 'msgstr "' + m[1] + '"');
+                }
+                else 
+                {
+                    mergedFile += match.value;
+                }
+            });
+            grunt.file.write(file, mergedFile);
+        });
+        done();
+    });
 };
