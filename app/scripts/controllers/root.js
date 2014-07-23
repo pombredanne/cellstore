a5cb372903aded80914eb298c69f0d07ab39595d
William Candillon <wcandillon@gmail.com>
2014-07-23 16:59:48 -0700

    Fix conflicts

--- app/scripts/controllers/root.js
+++ app/scripts/controllers/root.js
@@ -29,33 +29,21 @@ angular.module('main')
     });
 })
 .controller('RootFilingCtrl', function($state, $stateParams, $location, filing){
-<<<<<<< HEAD
-    var cik = filing.Archives[0].CIK.substring('http://www.sec.gov/CIK'.length + 1);
-=======
     filing = filing.data;
     var rut = filing.Archives[0].RUT.substring('http://www.svs.cl/rut'.length + 1);
->>>>>>> cik/CIK -> rut/RUT
     //This state is a redirect
     $state.go('root.entity.filing', { rut: rut, aid: $stateParams.aid });
     $location.replace();
 })
 .controller('RootComponentsCtrl', function($state, $stateParams, $location, components){
-<<<<<<< HEAD
-    var cik = components.Archives[0].CIK.substring('http://www.sec.gov/CIK'.length + 1);
-=======
     components = components.data;
     var rut = components.Archives[0].RUT.substring('http://www.svs.cl/rut'.length + 1);
->>>>>>> cik/CIK -> rut/RUT
     //This state is a redirect
     $state.go('root.entity.components', { rut: rut, aid: $stateParams.aid });
     $location.replace();
 })
 .controller('RootComponentCtrl', function($state, $stateParams, $location, component) {
-<<<<<<< HEAD
-    var cik = component.Archives[0].CIK.substring('http://www.sec.gov/CIK'.length + 1);
-=======
     var rut = component.data.Archives[0].RUT.substring('http://www.svs.cl/rut'.length + 1);
->>>>>>> cik/CIK -> rut/RUT
     var aid = $stateParams.aid;
     var networkIdentifier = $stateParams.networkIdentifier;
     //This state is a redirect
@@ -63,11 +51,7 @@ angular.module('main')
     $location.replace();
 })
 .controller('RootFactTableCtrl', function($state, $stateParams, $location, facttable){
-<<<<<<< HEAD
-    var cik = facttable.CIK.substring('http://www.sec.gov/CIK'.length + 1);
-=======
     var rut = facttable.RUT.substring('http://www.svs.cl/rut'.length + 1);
->>>>>>> cik/CIK -> rut/RUT
     var aid = $stateParams.aid;
     var networkIdentifier = $stateParams.networkIdentifier;
     //This state is a redirect
@@ -83,11 +67,7 @@ angular.module('main')
     $location.replace();
 })
 .controller('RootModelStructureCtrl', function($state, $stateParams, $location, modelStructure){
-<<<<<<< HEAD
-    var cik = modelStructure.CIK.substring('http://www.sec.gov/CIK'.length + 1);
-=======
     var rut = modelStructure.data.RUT.substring('http://www.svs.cl/rut'.length + 1);
->>>>>>> cik/CIK -> rut/RUT
     var aid = $stateParams.aid;
     var networkIdentifier = $stateParams.networkIdentifier;
     //This state is a redirect
