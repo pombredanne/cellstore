2fa0209bb84e0b8e7fa71101d6f653a8946a6e8d
Matthias Brantner <matthias.brantner@28msec.com>
2014-08-15 15:21:34 -0700

    fix stupid typo

--- Gruntfile.js
+++ Gruntfile.js
@@ -505,7 +505,7 @@ module.exports = function (grunt) {
     });
 
     grunt.registerTask('test', function (target) {
-        grunt.task.run(['decrypt', 'config']);
+        grunt.task.run(['shell:decrypt', 'config']);
         if (target === 'setup') {
             grunt.task.run([
                 'build',
