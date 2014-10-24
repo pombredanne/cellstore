2d3fe7e6e6b943f758ffceb8fefedfaeebda224a
Dennis Knochenwefel <d@28.io>
2014-10-24 15:00:42 +0200

    fixed profile update caching problem.

--- app/app.js
+++ app/app.js
@@ -108,7 +108,7 @@ angular.module('secxbrl', [
     
     $rootScope.$on('alert', function(event, title, message){
         $modal.open( {
-            template: '<div class="modal-header"><span ng-bind-html="object.title"></span><a class="close" ng-click="cancel()">&times;</a></div><div class="modal-body" ng-bind-html="object.message"></div><div class="text-right modal-footer"><button class="btn btn-default" ng-click="cancel()">OK</button></div>',
+            template: '<div class="modal-header"><span ng-bind-html="object.title" id="alert-header"></span><a class="close" ng-click="cancel()">&times;</a></div><div class="modal-body" ng-bind-html="object.message" id="alert-body"></div><div class="text-right modal-footer"><button class="btn btn-default" ng-click="cancel()">OK</button></div>',
             controller: ['$scope', '$modalInstance', 'object',  function ($scope, $modalInstance, object) {
                 $scope.object = object;
                 $scope.cancel = function () {
