537eef103211cd18a1d0a08c4203915510aaaefc
William Candillon <wcandillon@gmail.com>
2014-11-13 15:25:26 -0800

    Fix build

--- app/modules/ui/hero-unit.js
+++ app/modules/ui/hero-unit.js
@@ -4,18 +4,18 @@ angular
 .module('secxbrl')
 .directive('heroUnit', function($window, $rootScope){
 
-        var onScroll = function(event, toState) {
-            var top = document.body.getBoundingClientRect().top;
-            if(top >= 0 && ((document.getElementById('home') !== null && !toState) || (toState && toState.name === 'home.start'))) {
-                document.querySelector('nav.navbar').classList.add('transparent');
-            } else {
-                document.querySelector('nav.navbar').classList.remove('transparent');
-            }
-        };
+    var onScroll = function(event, toState) {
+        var top = document.body.getBoundingClientRect().top;
+        if(top >= 0 && ((document.getElementById('home') !== null && !toState) || (toState && toState.name === 'home.start'))) {
+            document.querySelector('nav.navbar').classList.add('transparent');
+        } else {
+            document.querySelector('nav.navbar').classList.remove('transparent');
+        }
+    };
 
-        angular.element($window).bind("scroll", onScroll);
-        onScroll();
-        $rootScope.$on('$stateChangeSuccess', onScroll);
+    angular.element($window).bind('scroll', onScroll);
+    onScroll();
+    $rootScope.$on('$stateChangeSuccess', onScroll);
 
     return function($scope, $element){
         var w = angular.element($window);
