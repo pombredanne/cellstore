b2fde9b5e4051fd4409b7245fe22c5aef2043464
William Candillon <wcandillon@gmail.com>
2014-11-12 21:54:18 -0800

    Minor improvements

--- app/modules/ui/hero-unit.js
+++ app/modules/ui/hero-unit.js
@@ -17,6 +17,17 @@ angular
             var title = document.querySelector('.title');
             var titleH = title.getBoundingClientRect().height;
             title.style.marginTop = ((newValue - titleH) / 2) + 'px';
+
+        });
+
+        angular.element($window).bind("scroll", function() {
+            var top = document.body.getBoundingClientRect().top;
+            console.log(top);
+            if(top >= 0) {
+                document.querySelector('nav.navbar').classList.add('transparent');
+            } else {
+                document.querySelector('nav.navbar').classList.remove('transparent');
+            }
         });
 
         w.bind('resize', function () {
