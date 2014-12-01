d6f19bd9c456b58635e9b90c78df31933cf32b8c
Dennis Knochenwefel <d@28.io>
2014-12-01 16:01:59 +0100

    more cleanup in swagger codegen.

--- tasks/swagger.js
+++ tasks/swagger.js
@@ -4,17 +4,7 @@ var fs = require('fs');
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
 
-var mkdirSync = function (path) {
-  try {
-    fs.mkdirSync(path);
-  } catch(e) {
-    if ( e.code !== 'EEXIST' ) { throw e; }
-  }
-};
-
 gulp.task('swagger', function(done){
-    var request = require('request');
-    var Q = require('q');
     var CodeGen = require('swagger-js-codegen').CodeGen;
     var apis = [
         {
@@ -42,7 +32,6 @@ gulp.task('swagger', function(done){
     ];
     var dest = 'app/modules';
     apis.forEach(function(api){
-        mkdirSync('swagger');
         var swagger = JSON.parse(fs.readFileSync(api.swagger, 'utf-8'));
         var source = CodeGen.getAngularCode({ moduleName: api.moduleName, className: api.className, swagger: swagger });
         $.util.log('Generated ' + api.moduleName + '.js from ' + api.swagger);
