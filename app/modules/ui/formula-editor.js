d38010ec2e4e39b3567d12a0d294faa3bbe368a6
Dennis Knochenwefel <d@28.io>
2014-12-17 16:22:13 +0100

    reorganized formula directives.

--- app/modules/ui/formula-editor.js
+++ app/modules/ui/formula-editor.js
@@ -2,70 +2,6 @@
 
 angular
     .module('report-editor')
-    .directive('formulas', function($modal, $log){
-        return {
-            restrict: 'E',
-            scope: {
-                'conceptName': '@',
-                'type': '@',
-                'report': '='
-            },
-            templateUrl: '/report/taxonomy/concept/formula/directive-formulas.html',
-            link: function($scope) {
-
-                $scope.hasComputingRule = false;
-                $scope.hasValidatingRules = false;
-                $scope.rulesList = undefined;
-
-                var updateRulesList = function(concept){
-                    if(concept === undefined || concept === null){
-                        $scope.rulesList = undefined;
-                        $scope.hasComputingRule = false;
-                        $scope.hasValidatingRules = false;
-                    } else if($scope.type === 'computation'){
-                        $scope.rulesList = $scope.report.listRules(concept);
-                        if($scope.rulesList.length > 0){
-                            $scope.hasComputingRule = true;
-                        } else {
-                            $scope.hasComputingRule = false;
-                        }
-
-                    } else if($scope.type === 'validation'){
-                        $scope.rulesList = $scope.report.listValidatingRules(concept);
-                        if($scope.rulesList.length > 0){
-                            $scope.hasValidatingRules = true;
-                        } else {
-                            $scope.hasValidatingRules = false;
-                        }
-                    } else {
-                        throw new Error('Unknown formula type ' + $scope.type + ' (computation or validation allowed).');
-                    }
-                };
-                updateRulesList($scope.conceptName);
-
-                $scope.$watch(function(){
-                    return $scope.report.listRules();
-                }, function(){
-                    updateRulesList($scope.conceptName);
-                }, true);
-
-                $scope.removeRule = function(id){
-                    var rule = $scope.report.getRule(id);
-                    var concept = rule.ComputableConcepts[0];
-                    if(rule.Type === 'xbrl28:validation' && rule.OriginalLanguage === 'SpreadsheetFormula' &&
-                        $scope.report.existsConcept(concept)) {
-                        try {
-                            $scope.report.removeConcept(concept);
-                        } catch(e){
-                            $log.log('Info: automatic removal of validation concept '+ concept + ' failed. Reason: ' + e.name + ': ' + e.message);
-                        }
-                    }
-                    $scope.report.removeRule(id);
-                };
-
-            }
-        };
-    })
     .directive('formulaEditor', function($state, Rule){
         return {
             restrict: 'E',
@@ -76,7 +12,7 @@ angular
                 'ruleId': '=',
                 'type': '@'
             },
-            templateUrl: '/report/taxonomy/concept/formula/directive-formula-editor.html',
+            templateUrl: '/modules/ui/formula-editor.html',
             link: function($scope) {
                 $scope.error = undefined;
                 if($scope.languageType === ''){
@@ -176,20 +112,6 @@ angular
                         $scope.availableConceptNames = $scope.formula.listAvailableConceptNames();
                         $scope.formula.validate($scope.action, true);
                     }
-                });
-            }
-        };
-    })
-    .directive('autoRecompileBindHtml', function($compile, $parse){
-        return {
-            link: function($scope, element, attr){
-                // Recompile the ng bind html
-                $scope.$watch(function() {
-                    var parsed = $parse(attr.ngBindHtml);
-                    var val = parsed($scope) || '';
-                    return val.toString();
-                }, function() {
-                    $compile(element, null, -9999 /*skip directives*/)($scope);
                 });
             }
         };
