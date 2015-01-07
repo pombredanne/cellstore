6773f1edbcf75081278b58c7c1e9de30df074c5a
William Candillon <wcandillon@gmail.com>
2015-01-06 21:40:47 -0800

    Update

--- app/api/api.js
+++ app/api/api.js
@@ -1,6 +1,49 @@
 'use strict';
 
 angular.module('report-editor')
-    .controller('APICtrl', function(){
+    .controller('APICtrl', function($scope, $state, $sce, $stateParams, swagger, API_URL, Session, API){
+        var method = $stateParams.method;
+        var path = '/' + $stateParams.path;
+        var operation;
+        swagger.apis.forEach(function(api){
+            if(api.path === path) {
+                api.operations.forEach(function(op){
+                    if(op.method === method) {
+                        operation = op;
+                        return false;
+                    }
+                });
+                return false;
+            }
+        });
 
+        if(!operation) {
+            $state.go('404');
+            return;
+        }
+
+        console.log(operation);
+        operation.notes = $sce.trustAsHtml(operation.notes);
+        operation.parameters = _.chain(operation.parameters).filter(function(param){
+            return param.name !== 'token' && param.name !== '_method';
+        }).forEach(function(param){
+            param.description = $sce.trustAsHtml(param.description);
+        }).value();
+
+        operation.examples.forEach(function(example){
+            example.description = $sce.trustAsHtml(example.description);
+        });
+
+        $scope.API_URL = API_URL;
+        $scope.api = swagger;
+        $scope.path = path;
+        $scope.op = operation;
+        $scope.params = {
+            token: Session.getToken()
+        };
+        console.log(operation);
+
+        $scope.test = function(){
+            console.log($scope.params);
+        };
     });
--- app/api/config.js
+++ app/api/config.js
@@ -5,9 +5,20 @@ angular
     .config(function ($stateProvider) {
         $stateProvider
             .state('api', {
-                url: '/api',
+                url: '/api/:method/*path',
                 templateUrl: '/api/api.html',
-                controller: 'APICtrl'
+                controller: 'APICtrl',
+                resolve: {
+                    swagger: ['$http', function($http){
+                        return $http({
+                            method: 'GET',
+                            url: '/swagger/queries.json',
+                            cache: true
+                        }).then(function(response){
+                            return response.data;
+                        });
+                    }]
+                }
             });
     })
 ;
